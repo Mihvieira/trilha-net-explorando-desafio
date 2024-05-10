0407083091fbd9afddf963e3add296af8c137394
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-05-10 11:14:50 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+using System.Net.WebSockets;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -6,17 +7,20 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Mirelly", sobrenome: "Vieira");
+Pessoa p2 = new Pessoa(nome: "Maria", sobrenome: "Santos");
+
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
+
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Standard", capacidade: 4, valorDiaria: 100);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 11);
+
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
